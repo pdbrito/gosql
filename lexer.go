ca41b002137203efde3229bcd82a77c6405b2f7f
Pedro de Brito <ped.de.brito@gmail.com>
2020-05-12 13:21:24 +0200

    Add lex() function

--- lexer.go
+++ lexer.go
@@ -1,5 +1,9 @@
 package gosql
 
+import (
+	"fmt"
+)
+
 type location struct {
 	line uint
 	col  uint
@@ -56,3 +60,33 @@ func (t *token) equals(other *token) bool {
 }
 
 type lexer func(string, cursor) (*token, cursor, bool)
+
+func lex(source string) ([]*token, error) {
+	var tokens []*token
+	cur := cursor{}
+
+lex:
+	for cur.pointer < uint(len(source)) {
+		lexers := []lexer{lexKeyword, lexSymbol, lexString, lexNumeric, lexIdentifier}
+		for _, l := range lexers {
+			if token, newCursor, ok := l(source, cur); ok {
+				cur = newCursor
+
+				// Omit nil tokens for valid but empty syntax like newlines
+				if token != nil {
+					tokens = append(tokens, token)
+				}
+
+				continue lex
+			}
+		}
+
+		hint := ""
+		if len(tokens) > 0 {
+			hint = "after " + tokens[len(tokens)-1].value
+		}
+		return nil, fmt.Errorf("unable to lex token %s, at %d:%d", hint, cur.loc.line, cur.loc.col)
+	}
+
+	return tokens, nil
+}
