2b407dbdb3e89f5efc58aca5a8dd0dddd1ec1958
Pedro de Brito <ped.de.brito@gmail.com>
2020-05-26 10:51:00 +0200

    Add parseInsertStatement()

---
+++ parse_insert.go
@@ -0,0 +1,53 @@
+package gosql
+
+func parseInsertStatement(tokens []*token, initialCursor uint, delimiter token) (*InsertStatement, uint, bool) {
+	cursor := initialCursor
+	if !expectToken(tokens, cursor, tokenFromKeyword(insertKeyword)) {
+		helpMessage(tokens, cursor, "expected INSERT")
+		return nil, initialCursor, false
+	}
+	cursor++
+
+	if !expectToken(tokens, cursor, tokenFromKeyword(intoKeyword)) {
+		helpMessage(tokens, cursor, "expected INTO")
+		return nil, initialCursor, false
+	}
+	cursor++
+
+	table, newCursor, ok := parseToken(tokens, cursor, identifierKind)
+	if !ok {
+		helpMessage(tokens, cursor, "expected table name")
+		return nil, initialCursor, false
+	}
+	cursor = newCursor
+
+	if !expectToken(tokens, cursor, tokenFromKeyword(valuesKeyword)) {
+		helpMessage(tokens, cursor, "expected VALUES")
+		return nil, initialCursor, false
+	}
+	cursor++
+
+	if !expectToken(tokens, cursor, tokenFromSymbol(leftParenSymbol)) {
+		helpMessage(tokens, cursor, "expected (")
+		return nil, initialCursor, false
+	}
+	cursor++
+
+	values, newCursor, ok := parseExpressions(tokens, cursor, []token{tokenFromSymbol(rightParenSymbol)})
+	if !ok {
+		helpMessage(tokens, cursor, "expected one or more comma separated values")
+		return nil, initialCursor, false
+	}
+	cursor = newCursor
+
+	if !expectToken(tokens, cursor, tokenFromSymbol(rightParenSymbol)) {
+		helpMessage(tokens, cursor, "expected )")
+		return nil, initialCursor, false
+	}
+	cursor++
+
+	return &InsertStatement{
+		table:  *table,
+		values: values,
+	}, cursor, true
+}
