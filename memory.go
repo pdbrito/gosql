51d443321badbc305c949dbc3c034ee71e3b9300
Pedro de Brito <ped.de.brito@gmail.com>
2020-05-31 09:22:16 +0200

    Add CreateTable() to MemoryBackend

--- memory.go
+++ memory.go
@@ -68,3 +68,29 @@ type MemoryBackend struct {
 func NewMemoryBackend() *MemoryBackend {
 	return &MemoryBackend{tables: map[string]*table{}}
 }
+
+func (mb *MemoryBackend) CreateTable(crt *CreateTableStatement) error {
+	t := table{}
+	mb.tables[crt.name.value] = &t
+	if crt.cols == nil {
+		return nil
+	}
+
+	for _, col := range *crt.cols {
+		t.columns = append(t.columns, col.name.value)
+
+		var dt ColumnType
+		switch col.datatype.value {
+		case "int":
+			dt = IntType
+		case "text":
+			dt = TextType
+		default:
+			return ErrInvalidDatatype
+		}
+
+		t.columnTypes = append(t.columnTypes, dt)
+	}
+
+	return nil
+}
