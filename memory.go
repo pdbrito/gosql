cf93e9905edfdcb4daf7e94a03281feadf6f8d7b
Pedro de Brito <ped.de.brito@gmail.com>
2020-06-01 10:58:47 +0200

    Add Select() to MemoryBackend

--- memory.go
+++ memory.go
@@ -144,3 +144,108 @@ func (mb *MemoryBackend) tokenToCell(t *token) MemoryCell {
 	}
 	return nil
 }
+
+func (mb *MemoryBackend) Select(slct *SelectStatement) (*Results, error) {
+	table := &table{}
+
+	if slct.from != nil && slct.from.table != nil {
+		var ok bool
+		table, ok = mb.tables[slct.from.table.value]
+		if !ok {
+			return nil, ErrTableDoesNotExist
+		}
+	}
+
+	if slct.item == nil || len(*slct.item) == 0 {
+		return &Results{}, nil
+	}
+
+	results := [][]Cell{}
+	columns := []struct {
+		Type ColumnType
+		Name string
+	}{}
+
+	if slct.from == nil {
+		result := []MemoryCell{}
+		for _, col := range *slct.item {
+			nonImmediateLiteral := !col.asterisk && col.exp.kind == literalKind && !(col.exp.literal.kind == numericKind)
+			if nonImmediateLiteral || col.asterisk || col.exp.kind != literalKind {
+				return nil, ErrInvalidSelectItem
+			}
+
+			result = append(result, mb.tokenToCell(col.exp.literal))
+		}
+	}
+
+	for _, row := range table.rows {
+		result := []Cell{}
+
+		for _, col := range *slct.item {
+			if col.asterisk {
+				// TODO: handle asterisk
+				fmt.Println("Skipping asterisk.")
+				continue
+			}
+
+			exp := col.exp
+			if exp.kind != literalKind {
+				// Unsupported, doesn't currently exist, ignore.
+				fmt.Println("Skipping non-literal expression.")
+				continue
+			}
+
+			lit := exp.literal
+			if lit.kind == identifierKind {
+				found := false
+				for i, tableCol := range table.columns {
+					if tableCol == lit.value {
+						columns = append(columns, struct {
+							Type ColumnType
+							Name string
+						}{
+							Type: table.columnTypes[i],
+							Name: lit.value,
+						})
+
+						result = append(result, row[i])
+						found = true
+						break
+					}
+				}
+
+				if !found {
+					return nil, ErrColumnDoesNotExist
+				}
+
+				continue
+			}
+
+			if lit.kind == numericKind || lit.kind == stringKind {
+				columnType := IntType
+				if lit.kind == stringKind {
+					columnType = TextType
+				}
+
+				columns = append(columns, struct {
+					Type ColumnType
+					Name string
+				}{
+					Type: columnType,
+					Name: col.exp.literal.value,
+				})
+				result = append(result, mb.tokenToCell(lit))
+				continue
+			}
+
+			return nil, ErrColumnDoesNotExist
+		}
+
+		results = append(results, result)
+	}
+
+	return &Results{
+		Columns: columns,
+		Rows:    results,
+	}, nil
+}
